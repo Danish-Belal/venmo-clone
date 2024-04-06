0582eaef3be02b6c823a761de86060076983e6cd
Danish-Belal <danishexplore019@gmail.com>
2024-04-06 23:01:39 +0530

    Route

--- apps/user-app/app/api/user/route.ts
+++ apps/user-app/app/api/user/route.ts
@@ -3,15 +3,20 @@ import { NextResponse } from "next/server";
 import { authOptions } from "../../lib/auth";
 
 export const GET = async () => {
+    try{
     const session = await getServerSession(authOptions);
     if (session.user) {
         return NextResponse.json({
             user: session.user
         })
     }
-    return NextResponse.json({
-        message: "You are not logged in"
-    }, {
-        status: 403
-    })
+    }
+    catch(e){
+        return NextResponse.json({
+            message: "You are not logged in"
+        }, {
+            status: 403
+        })
+    }
+   
 }
