78d3db62fe4e1927a3e3faaee725bec4fe7736c6
Danish-Belal <danishexplore019@gmail.com>
2024-04-08 02:17:21 +0530

    card

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -8,13 +8,9 @@ export function Card({
   children?: React.ReactNode;
 }): JSX.Element {
   return (
-    <div
-      className="border p-4"
-    >
-      <h1 className="text-xl border-b pb-2">
-        {title}
-      </h1>
-      <p>{children}</p>
+    <div className="border p-6 bg-white rounded-xl bg-[#ededed]">
+      <h1 className="text-xl border-b pb-2">{title}</h1>
+      {children} {/* Render children directly */}
     </div>
   );
 }
