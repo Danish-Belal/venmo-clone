619649e42982f30421a33f094b6fb2cb6ac7ab21
Danish-Belal <danishexplore019@gmail.com>
2024-04-14 22:26:33 +0530

    Fetching balance form db

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import db from "@repo/db/client";
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
@@ -10,46 +10,55 @@ app.post("/hdfcWebhook", async (req, res) => {
     const paymentInformation: {
         token: string;
         userId: string;
-        amount: string
+        amount: string;
     } = {
         token: req.body.token,
         userId: req.body.user_identifier,
-        amount: req.body.amount
+        amount: req.body.amount,
     };
 
     try {
+        const existingBalance = await db.balance.findFirst({
+            where: {
+                userId: Number(paymentInformation.userId),
+            },
+        });
+
+        if (!existingBalance) {
+            throw new Error("Balance not found for user");
+        }
+
+        const currentBalance = existingBalance.amount;
+        const updatedBalance = currentBalance + Number(paymentInformation.amount);
+
         await db.$transaction([
             db.balance.updateMany({
                 where: {
-                    userId: Number(paymentInformation.userId)
+                    userId: Number(paymentInformation.userId),
                 },
                 data: {
-                    amount: {
-                        // You can also get this from your DB
-                        increment: Number(paymentInformation.amount)
-                    }
-                }
+                    amount: updatedBalance,
+                },
             }),
             db.onRampTransaction.updateMany({
                 where: {
-                    token: paymentInformation.token
-                }, 
+                    token: paymentInformation.token,
+                },
                 data: {
                     status: "Success",
-                }
-            })
+                },
+            }),
         ]);
 
         res.json({
-            message: "Captured"
-        })
-    } catch(e) {
+            message: "Captured",
+        });
+    } catch (e) {
         console.error(e);
         res.status(411).json({
-            message: "Error while processing webhook"
-        })
+            message: "Error while processing webhook",
+        });
     }
-
-})
+});
 
 app.listen(3003);
