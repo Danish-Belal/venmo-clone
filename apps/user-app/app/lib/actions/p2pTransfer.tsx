712224cf80f0eb6e4be3247f394362fb3de4ab6d
Danish-Belal <danishexplore019@gmail.com>
2024-04-20 22:14:44 +0530

    P2P

--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -41,5 +41,14 @@ export async function p2pTransfer(to: string, amount: number) {
             where: { userId: toUser.id },
             data: { amount: { increment: amount } },
           });
+
+          await tx.p2pTransfer.create({
+            data:{
+                fromUserId: Number(from),
+                toUserId: toUser.id,
+                amount,
+                timestamp: new Date()
+            }
+          })
     });
 }
