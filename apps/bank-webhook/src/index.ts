5d9e17ac5461911d77b517e81618a6459275fb28
Danish-Belal <danishexplore019@gmail.com>
2024-04-20 22:14:11 +0530

    Web hook

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -7,6 +7,7 @@ app.use(express.json());
 app.post("/hdfcWebhook", async (req, res) => {
     //TODO: Add zod validation here?
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+    // TODO: check if the onramp is processing or not
     const paymentInformation: {
         token: string;
         userId: string;
