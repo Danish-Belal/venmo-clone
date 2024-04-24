12cab7a735b730c232819e0f629d02874280fe10
Danish-Belal <danishexplore019@gmail.com>
2024-04-24 22:20:40 +0530

    page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,12 +1,13 @@
 import { getServerSession } from "next-auth";
 import { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
+import Login from "@repo/ui/login";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
   } else {
-    redirect('/api/auth/signin')
+    redirect('/api/signin')
   }
 }
