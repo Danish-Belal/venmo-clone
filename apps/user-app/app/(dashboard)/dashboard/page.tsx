6852faa865b5e46d8a14a0e0008d531883871dc9
Danish-Belal <danishexplore019@gmail.com>
2024-04-25 20:47:42 +0530

    Modified Dashboard

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,12 +1,23 @@
+
 import { authOptions } from "../../lib/auth";
 import { getServerSession } from "next-auth";
+import DashboardCard from "../../../components/DashboardCard"
+import {getBalance} from "../transfer/page"
 
 export default async function() {
     const session = await getServerSession(authOptions);
-    // console.log(session);
+    const transactions = await getBalance();
+   
+    console.log(transactions.amount);
+
     
-     return <div>
-        DashBoard<br/>
-         {session.user.email}
-     </div>
+    return (
+        <div>
+        <div className="text-center text-2xl font-semibold mb-4">Good morning, {session.user.email}</div>
+        <div className="bg-gray-100 px-4 py-8">
+        <DashboardCard title="Portfolio Value " value={transactions.amount || 0} ₹ />
+        {/* <DashboardCard title="User Balance" value={balance || 0} /> */}
+    </div>
+    </div>
+    );
  }
