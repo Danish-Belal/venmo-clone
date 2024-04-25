28b8efb5ce6c2eff3a64dffb1893a8f398be98f1
Danish-Belal <danishexplore019@gmail.com>
2024-04-25 20:46:48 +0530

    Dashboard Card

---
+++ apps/user-app/components/DashboardCard.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DashboardCard = ({ title, value }) => {
+    return (
+        <div className="bg-white overflow-hidden shadow rounded-lg">
+            <div className="px-4 py-5 sm:p-6">
+                <div className="text-center">
+                    <span className="text-sm font-medium text-gray-500">{title}</span>
+                </div>
+                <div className="mt-2 text-center">
+                    <p className="text-3xl font-semibold text-gray-900">{value}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardCard;
