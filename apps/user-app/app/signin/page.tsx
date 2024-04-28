b5f865dac891071d10fd4fcf69f8aca8c8016640
Danish-Belal <danishexplore019@gmail.com>
2024-04-28 22:51:30 +0530

    Signin Page

--- apps/user-app/app/signin/page.tsx
+++ apps/user-app/app/signin/page.tsx
@@ -1,8 +1,32 @@
 
 "use client"
 import Head from 'next/head';
+import { useState } from 'react';
+import { signIn    } from "next-auth/react";
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (e:any) => {
+    e.preventcdDefault();
+
+    try {
+      const result = await signIn('credentials', {
+        phone: username,
+        password
+        // redirect: true, // Prevent NextAuth from redirecting automatically
+        // callbackUrl: '/dashboard'
+      });
+
+      if (result.error) {
+        setErrorMessage('Invalid username or password. Please try again.');
+      }
+    } catch (error) {
+      console.error('Failed to sign in:', error);
+    }
+  };
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <Head>
@@ -29,6 +53,7 @@ export default function Login() {
                   type="text"
                   autoComplete="username"
                   required
+                  onChange={(e) => setUsername(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
@@ -45,18 +70,27 @@ export default function Login() {
                   type="password"
                   autoComplete="current-password"
                   required
+                  onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
             </div>
 
+            {errorMessage && (
+              <p className="mt-2 text-sm text-red-600" id="error-message">
+                {errorMessage}
+              </p>
+            )}
+
+
             <div>
               <button
                 type="submit"
+                onClick={handleSubmit}
                 className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Sign in
-              </button>
+              </button >
             </div>
           </form>
         </div>
