8ac44eb99b81e91d86de163f7e2235aa6c2d75c9
Danish-Belal <danishexplore019@gmail.com>
2024-05-19 23:03:59 +0530

    Dashboard modification

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { authOptions } from "../../lib/auth";
 import { getServerSession } from "next-auth";
 import DashboardCard from "../../../components/DashboardCard"
 import {getBalance, getOnRampTransactions} from "../transfer/page"
+import dashboard from "../../../public/Image.png"
 
 export default async function() {
     const session = await getServerSession(authOptions);
@@ -16,10 +17,14 @@ export default async function() {
     
     return (
         <div>
-         <div className="text-center text-2xl font-semibold mb-4 text-blue-500">Good morning, <span className="text-blue-500">{session.user.email}</span></div>
-        <div className="bg-gray-100  ">
-        <DashboardCard title="Portfolio Value " value={transactions.amount || 0}  transactions= {allTransactions}/>
+         <div className="text-center text-2xl font-semibold mb-4 text-blue-500">Good Day, <span className="text-blue-500">{session.user.email}</span></div>
+
+        
+    <DashboardCard title="Portfolio Value " value={transactions.amount || 0}  transactions= {allTransactions}/>
+    <div className="bg-gray-100  ">
+        <img src={dashboard.src} alt="Description of the image" />
     </div>
     </div>
+    
     );
  }
