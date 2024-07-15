f8c8f5fcfe0e57c328bace8c53428177f992d5a9
Danish-Belal <danishexplore019@gmail.com>
2024-07-15 22:55:11 +0530

    Modified webhook

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.json());
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
+    //TODO: Add zod validation here
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
-    // TODO: check if the onramp is processing or not
+    //TODO: check if the onramp is processing or not
     const paymentInformation: {
         token: string;
         userId: string;
@@ -19,14 +19,21 @@ app.post("/hdfcWebhook", async (req, res) => {
     };
 
     try {
-        const existingBalance = await db.balance.findFirst({
+        let existingBalance = await db.balance.findFirst({
             where: {
                 userId: Number(paymentInformation.userId),
             },
         });
 
         if (!existingBalance) {
-            throw new Error("Balance not found for user");
+            existingBalance = await db.balance.create({
+                data: {
+                    userId: Number(paymentInformation.userId),
+                    amount: 0,
+                    locked: 0
+                    
+                },
+            });
         }
 
         const currentBalance = existingBalance.amount;
@@ -56,10 +63,13 @@ app.post("/hdfcWebhook", async (req, res) => {
         });
     } catch (e) {
         console.error(e);
-        res.status(411).json({
-            message: `Error while processing webhook`,e,
+        res.status(500).json({
+            message: `Error while processing webhook"`,e,
+            
         });
     }
 });
 
-app.listen(3003);
+app.listen(3003, () => {
+    console.log("Server is running on port 3003");
+});
